ade72b80d5e24586ade7b6b3f87f952cb1197317
Marshall <Marshall@creativeideal.net>
2020-06-08 23:03:42 -0600

    Test buttons

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,33 @@
 <template>
   <div>
-    <button @click="e => test()">Test</button>
+    <button @click="e => fn()">Fn</button>
+    <button @click="e => nested.fn()">Nested Fn</button>
   </div>
 </template>
 
 <script>
 import { ref } from 'vue'
 
+function fn() {
+  console.log(Object.keys(this))
+}
+
 export default {
   name: 'App',
   setup() {
-    const otherProp = ref(null)
-    function test() {
-      console.log(Object.keys(this))
+    const someRef = ref(null)
+
+    const nested = {
+      fn() {
+        console.log(Object.keys(this))
+      },
+      prop: true
     }
+
     return {
-      otherProp,
-      test
+      someRef,
+      fn,
+      nested
     }
   }
 }
