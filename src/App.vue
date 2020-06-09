d6d84303b6f08eb455b3dde8e0a3c3fd6505058c
Marshall <Marshall@creativeideal.net>
2020-06-08 23:05:41 -0600

    Add some comments

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,26 @@
 <template>
   <div>
     <button @click="e => fn()">Fn</button>
-    <button @click="e => nested.fn()">Nested Fn</button>
+    <button @click="e => nested.util()">Nested Fn</button>
   </div>
 </template>
 
 <script>
 import { ref } from 'vue'
 
-function fn() {
-  console.log(Object.keys(this))
-}
-
 export default {
   name: 'App',
   setup() {
     const someRef = ref(null)
 
+    // this === vm
+    function fn() {
+      console.log(Object.keys(this)) // --> ['someRef', 'fn', 'nested']
+    }
+
     const nested = {
-      fn() {
-        console.log(Object.keys(this))
+      util() {
+        console.log(Object.keys(this)) // --> ['util', 'prop']
       },
       prop: true
     }
