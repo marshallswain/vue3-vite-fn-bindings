523c9dfd131c8b725fff7128527b8d909b1eb55d
Marshall <Marshall@creativeideal.net>
2020-06-08 22:53:56 -0600

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <button @click="e => test()">Test</button>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  name: 'App',
+  setup() {
+    const otherProp = ref(null)
+    function test() {
+      console.log(Object.keys(this))
+    }
+    return {
+      otherProp,
+      test
+    }
+  }
+}
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <button>Test</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    model: Object
+  },
+  setup(props, context) {
+    const Test = props.model
+    console.log(Test)
+    return {}
+  }
+}
+</script>
